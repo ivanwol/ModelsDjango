5de409a48d9c75748200aac9c5764d90e1ab2134
ivan4 <ivanwolnoe@gmail.com>
2022-07-27 19:35:25 +0300

    CRUD operations views(update, delete)

--- ModelsDjango/urls.py
+++ ModelsDjango/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('create/', views.create),
+    path('edit/<int:id>', views.edit),
+    path('delete/<int:id>', views.delete),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from .models import Person
 
 
@@ -17,3 +17,29 @@ def create(request):
         people.phone = request.POST.get('phone')
         people.save()
     return HttpResponseRedirect('/')
+
+
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+
+        if request.method == 'POST':
+            person.name = request.POST.get('name')
+            person.surname = request.POST.get('surname')
+            person.age = request.POST.get('age')
+            person.phone = request.POST.get('phone')
+            person.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'edit.html', {'person': person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Page not found</h2>')
+
+
+def delete(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect('/')
+    except Person.DoesNotExist:
+        return HttpResponseNotFound('<h2>Page not found</h2>')
